1567c453c6668a3735a1a485eca7b76fb257cabe
Richard <legalize@xmission.com>
2023-12-13 10:16:56 -0700

    Catch exceptions in main()

--- tools/main.cpp
+++ tools/main.cpp
@@ -1,11 +1,26 @@
 #include "card.h"
 
 #include <algorithm>
+#include <exception>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     std::vector<std::string_view> args;
     args.resize(argc);
     std::copy(&argv[0], &argv[argc], args.begin());
-    return card::main(args);
+    try
+    {
+        return card::main(args);
+    }
+    catch (const std::exception &bang)
+    {
+        std::cerr << "Caught exception: " << bang.what() << '\n';
+        return 1;
+    }
+    catch (...)
+    {
+        std::cerr << "Unknown exception\n";
+        return 1;
+    }
 }
